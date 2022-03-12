e1822442396e2de655db0637ee003f073d6d023c
Erick Moore <erick@prosimo.io>
2022-03-12 15:33:50 -0500

    working on kv

--- prosimo/PowerShell/onboard-cloud-account.ps1
+++ prosimo/PowerShell/onboard-cloud-account.ps1
@@ -7,9 +7,15 @@ param(
   [string] [Parameter(Mandatory=$true)] $tenantId
 )
 
-#$Response = Invoke-RestMethod -Uri 'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fvault.azure.net' -Method GET -Headers @{Metadata="true"}
-#$KeyVaultToken = $Response.access_token
-#Invoke-RestMethod -Uri https://<your-key-vault-URL>/secrets/<secret-name>?api-version=2016-10-01 -Method GET -Headers @{Authorization="Bearer $KeyVaultToken"}
+$vaultClientId = 'https://' + $keyVaultName + 'vault.azure.net/secrets/' + $clientId + '?api-version=2016-10-01'
+$vaultClientSecret = 'https://' + $keyVaultName + 'vault.azure.net/secrets/' + $clientId + '?api-version=2016-10-01'
+
+$Response = Invoke-RestMethod -Uri 'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fvault.azure.net' -Method GET -Headers @{Metadata="true"}
+$KeyVaultToken = $Response.access_token
+
+$clientId = Invoke-RestMethod -Uri $vaultClientId -Method GET -Headers @{Authorization="Bearer $KeyVaultToken"}
+$clientSecret = Invoke-RestMethod -Uri $vaultClientId -Method GET -Headers @{Authorization="Bearer $KeyVaultToken"}
+prosimoApiToken = Invoke-RestMethod -Uri $vaultClientId -Method GET -Headers @{Authorization="Bearer $KeyVaultToken"}
 
   Install-Module -Name Az.ResourceGraph -Force
   $subscriptionList = (Search-AzGraph -Query "ResourceContainers | where type =~ 'microsoft.resources/subscriptions'" -ManagementGroup $managementGroupName).id

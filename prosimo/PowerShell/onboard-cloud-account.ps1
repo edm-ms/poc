75a92bf4747a28ee6c0e62adecb123de3c0af6f6
Erick Moore <erick@prosimo.io>
2022-03-14 15:53:42 -0400

    removed double quotes

--- prosimo/PowerShell/onboard-cloud-account.ps1
+++ prosimo/PowerShell/onboard-cloud-account.ps1
@@ -8,11 +8,11 @@ param(
   [string] [Parameter(Mandatory=$true)] $keyVaultName
 )
 
-$vaultUrl = "https://$keyVaultName.vault.azure.net"
+$vaultUrl = 'https://$keyVaultName.vault.azure.net'
 
-$clientSecretUri = $vaultUrl + \\"secrets\\" + $clientId + \\"?api-version=2016-10-01\\"
-$spSecretURI = $vaultUrl + \'//secrets//\' + $clientSecret + \'?api-version=2016-10-01\'
-$prosimoApiSecretURI = $vaultUrl + \"/secrets/\" + $prosimoApiToken + '?api-version=2016-10-01'
+$clientSecretUri = $vaultUrl + '//secrets//' + $clientId + '?api-version=2016-10-01'
+$spSecretURI = $vaultUrl + '//secrets//' + $clientSecret + '?api-version=2016-10-01'
+$prosimoApiSecretURI = $vaultUrl + '//secrets//' + $prosimoApiToken + '?api-version=2016-10-01'
 
 $Response = Invoke-RestMethod -Uri 'http://169.254.169.254//metadata//identity//oauth2//token?api-version=2018-02-01&resource=https%3A%2F%2Fvault.azure.net' -Method GET -Headers @{Metadata="true"}
 $KeyVaultToken = $Response.access_token
